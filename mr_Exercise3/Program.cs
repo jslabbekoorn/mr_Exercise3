d3e49c9a135a97dfffceffb08ed1bd977f10a846
John Slabbekoorn <john.slabbekoorn@gmail.com>
2013-03-25 21:53:09 +0100

    New

---
+++ mr_Exercise3/Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace mr_Exercise3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string strValue;
+            string strReversed;
+
+            Console.WriteLine("Palindroom checker v.2");
+
+            do
+            {
+                Console.Write("Voer een woord in: ");
+                strValue = Console.ReadLine().ToLower();
+
+                if (strValue != "")
+                {
+                    strReversed = Reverse(strValue);
+
+                    if (strValue == strReversed)
+                    {
+                        Console.WriteLine("Palindroom!");
+                    }
+                    else
+                    {
+                        Console.WriteLine("Geen Palindroom");
+                    }
+                }
+
+            } while (strValue != "");
+
+            Console.ReadLine();
+        }
+
+
+        private static string Reverse(string strValue)
+        {
+            char[] chArray = strValue.ToCharArray();
+            Array.Reverse(chArray);
+            return new string(chArray);
+
+        }
+    }
+}
